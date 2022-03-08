f764f2791cc7df77b65506bc97c45bb685682dc6
jaybeltran812 <94095944+jaybeltran812@users.noreply.github.com>
2022-03-08 16:52:02 +0800

    Update winrm.ps1

--- windows/winrm/winrm.ps1
+++ windows/winrm/winrm.ps1
@@ -4,3 +4,4 @@ winrm set winrm/config '@{MaxTimeoutms="1800000"}';
 winrm set winrm/config/service '@{AllowUnencrypted="true"}';
 winrm set winrm/config/service/auth '@{Basic="true"}';
 netsh advfirewall firewall add rule name=”WinRM-HTTP” dir=in localport=5985 protocol=TCP action=allow;
+netsh advfirewall firewall add rule name=”WinRM-HTTPS” dir=in localport=5986 protocol=TCP action=allow;
